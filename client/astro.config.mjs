f22b4335dfe4b2ddd7823971308cffeb244d4c78
Marat200118 <marat.samigullin1@gmail.com>
2023-11-28 16:00:23 +0100

    routing added

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   image: {
-    domains: ["http://localhost:1337"],
+    domains: ["localhost"],
   },
 });
